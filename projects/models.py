7a4d1e5812928af12ff586cda26ce09790b678f2
Kieron Woodhouse <kieron.woodhouse@hds.com>
2016-06-23 11:56:46 +0100

    Setup django for project

---
+++ projects/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.validators import MinValueValidator
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=240)
+    description = models.TextField()
+    author = models.CharField(max_length=50)
+    participants = models.TextField()
+    min_participants = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    max_participants = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    hackathon = models.ForeignKey('Hackathon', on_delete=models.SET_NULL, null=True)
+    skills = models.TextField()
+    tags = models.TextField()
+
+class Hackathon(models.Model):
+    number = models.IntegerField(primary_key=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
