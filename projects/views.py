01e1853261cbcc194e0a5ab95d9a47d33b11de05
Kieron Woodhouse <kieron.woodhouse@hds.com>
2016-06-23 14:00:32 +0100

    Adding additional views and urls

--- projects/views.py
+++ projects/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse("List of projects")
+
+def project(request, project_id):
+    return HttpResponse("Project %s" % project_id)
+
+def add_project(request):
+    return HttpResponse("Adding project")
+
+def edit_project(request, project_id):
+    return HttpResponse("Edit project %s" % project_id)
