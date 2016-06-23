c6aced86513cf94da9bb8f4d82ec4471bc295b02
Matthew-Bird <matthew.james.bird@gmail.com>
2016-06-23 14:34:07 +0100

    rename 'new' page to 'add'

--- projects/urls.py
+++ projects/urls.py
@@ -6,6 +6,6 @@ app_name = 'projects'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<project_id>[0-9]+)/$', views.project, name='project'),
-    url(r'^new/$', views.add_project, name='add_project'),
+    url(r'^add/$', views.add_project, name='add_project'),
     url(r'^edit/(?P<project_id>[0-9]+)/$', views.edit_project, name='edit_project')
 ]
