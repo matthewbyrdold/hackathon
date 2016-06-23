e21c59fadea811568cacf5a51b0e1f8b97e24c64
Matthew-Bird <matthew.james.bird@gmail.com>
2016-06-23 13:17:29 +0100

    added missing python 2 unicode include

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.core.validators import MinValueValidator
 
 @python_2_unicode_compatible
