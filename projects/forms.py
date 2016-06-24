04f5726ed396dde69aadf5b16d64a894339e0b2a
Kieron Woodhouse <kieron.woodhouse@hds.com>
2016-06-24 10:48:39 +0100

    Restructuring forms to reduce duplication

--- projects/forms.py
+++ projects/forms.py
@@ -4,4 +4,4 @@ from projects.models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model  = Project
-        fields = ['name', 'description', 'author', 'min_participants', 'max_participants', 'skills', 'tags']
+        fields = ['name', 'hackathon', 'description', 'author', 'min_participants', 'max_participants', 'skills', 'tags']
